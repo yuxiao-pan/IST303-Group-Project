4f1df172e9918dd71ce61cba781cc37f94a83460
nash90 <nash.rakuten@gmail.com>
2018-10-14 17:42:23 -0700

    Initialized the virtual environment with Django framework

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/abinash/Desktop/CGU/Courses/Software Development/project/IST303-Group-Project/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
