013e85f0c07ca1cb649b9382c8beeb36c975c013
nash90 <nash.rakuten@gmail.com>
2018-10-29 12:40:45 -0700

    implemented user signup and user login features

--- app/news/views.py
+++ app/news/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 from django.http import HttpResponse
 
 
-def index(request):
+def health(request):
     return HttpResponse("Application news portal Started", content_type="text/plain")
+
+def home(request):
+    return render(request, 'home.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def logout(request):
+    return render(request, 'dashboard.html')
--- app/test/test_auth_endpoint.py
+++ app/test/test_auth_endpoint.py
@@ -3,7 +3,7 @@ import pytest
 @pytest.mark.urls('news.urls')
 def test_index_page(client):
     expected = 'Application news portal Started'
-    result = client.get('/').content.decode("utf-8")
+    result = client.get('/health').content.decode("utf-8")
     assert expected in result
 
 
