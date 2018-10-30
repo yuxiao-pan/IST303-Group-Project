474d1ebf679125b900315066c0d16afcb771199b
nash90 <nash.rakuten@gmail.com>
2018-10-30 00:16:10 -0700

    impleted news list endbackend and frontend

--- app/news/views.py
+++ app/news/views.py
@@ -5,15 +5,31 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 
 from .service.authentication import Signup
+from .service.news_service import CategoryService
+from .service.news_service import NewsService
+from .models import Category
 
 def health(request):
     return HttpResponse("Application news portal Started", content_type="text/plain")
 
 def home(request):
-    return render(request, 'home.html')
+    categories = CategoryService().getAll()
+    news = NewsService().getAll()
+    
+    context = {
+        "categories":categories,
+        "news":getPreviewNews(news)
+    }
+    return render(request, 'home.html', context)
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    categories = CategoryService().getAll()
+    news = NewsService().getAll()
+    context = {
+        "categories":categories,
+        "news":news
+    }
+    return render(request, 'dashboard.html', context)
 
 def signup(request):
     signup = Signup()
@@ -25,7 +41,18 @@ def signup(request):
             return redirect('dashboard')
     else:
         form = signup.create_form()
-    return render(request, 'signup.html', {'form': form})
+
+    categories = CategoryService().getAll()
+    context = {
+        "categories":categories,
+        "form":form
+    }
+    return render(request, 'signup.html', context)
 
 def logout(request):
     return render(request, 'dashboard.html')
+
+def getPreviewNews(news):
+    for item in news:
+        item.content = item.content[0:200]
+    return news
